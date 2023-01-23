507852d3a92faad5cc9df3d31103f4ac7bc83fbb
sebastienmariette74 <61980470+sebastienmariette74@users.noreply.github.com>
2023-01-23 18:44:39 +0100

    modify registerPartnerController.php

--- src/Service/SendMailService.php
+++ src/Service/SendMailService.php
@@ -7,25 +7,26 @@ use Symfony\Component\Mailer\MailerInterface;
 
 class SendMailService 
 {
-    public function __construct(private MailerInterface $mailer ){}
+    // public function __construct( MailerInterface $mailer ){}
 
-    // public function send (
-    //     string $from,
-    //     string $to,
-    //     string $subject,
-    //     string $template,
-    //     array $context
-    // ): void
-    // {
-    //     $email = (new TemplatedEmail())
-    //                 ->from($from)
-    //                 ->to($to)
-    //                 ->subject($subject)
-    //                 ->htmlTemplate("email/$template.html.twig")
-    //                 ->context($context);
+    public function send (
+        MailerInterface $mailer,
+        string $from,
+        string $to,
+        string $subject,
+        string $template,
+        array $context
+    ): void
+    {
+        $email = (new TemplatedEmail())
+                    ->from($from)
+                    ->to($to)
+                    ->subject($subject)
+                    ->htmlTemplate("email/$template.html.twig")
+                    ->context($context);
 
-    //     $this->mailer->send($email);
-    // }
+        $mailer->send($email);
+    }
 }
 
 
